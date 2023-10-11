99b4b753933d8eab57273c9390095b67f01ee169
TwilightNightWing <88166572+TwilightNightWing@users.noreply.github.com>
2023-10-11 16:37:04 -0600

    Add files via upload

---
+++ proyecto152/js/base.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent("baseCarro", {
+
+
+    update: function() {
+        window.addEventListener("click", e =>{
+            this.data.clickCounter = this.data.clickCounter + 1;
+            if (this.data.clickCounter === 1){
+                const rotation = {x: 0, y:20, z:0};
+                this.el.setAttribute("rotation", rotation);
+            
+            }else if (this.data.clickCounter === 2){
+            const rotation = {x: 0, y: 100, z: 0}
+            }
+        })
+    }
+} )
